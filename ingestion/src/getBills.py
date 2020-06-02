510797877e5bd7ffd729481d5ded9f3a5ea35754
Leon's BigMac <liang268@purdue.edu>
2020-06-02 15:21:39 -0400

    refactor

--- ingestion/src/getBills.py
+++ ingestion/src/getBills.py
@@ -1,18 +1,24 @@
 import requests
 import xml.etree.ElementTree as ET
-
-
+from urllib.parse import urlparse
 
 # make a request to Library of Congress API for collections of Bills
 
 class getBills:
     def main():
         api_key = "api_key=oEA3hx5DO6bju4YuvyDP4H9eTqbn1T9G8nlkhur6"
-        r = requests.get("https://api.govinfo.gov/collections/BILLS/2008-01-01T00:00:00Z/?offset=0&pageSize=100&%s" % (api_key))
-        print(r.status_code)
+        r = requests.get("https://api.govinfo.gov/collections/BILLS/2008-01-01T00:00:00Z/?offset=0&pageSize=5&%s" % (api_key))
+        # print(r.status_code)
         packages = r.json()
-        print(packages)
+        # print(packages['packages'])
+        for p in packages['packages']:
+            sumLink = p['packageLink']
+            print(sumLink)
+            o = urlparse(sumLink)
+            print(o)
         # package1 = requests.get("https://api.govinfo.gov/packages/BILLS-115hr4403rh/xml?%s" % (api_key))
+        o = urlparse(sumLink)
+        print(o)
         # root = ET.fromstring(package1.text)
         # print(root.tag, root.attrib)
         # for child in root:
